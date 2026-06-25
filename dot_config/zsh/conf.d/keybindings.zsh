571b90c9ec9c94e891c27623bfc270e87a533640
kir-sth <kir.sth@yandex.ru>
2026-06-25 20:11:15 +0300

    chore: remove unused keybindings

--- dot_config/zsh/conf.d/keybindings.zsh
+++ dot_config/zsh/conf.d/keybindings.zsh
@@ -10,7 +10,5 @@ bindkey '^[b' backward-word      # Option+Left  (ESC+b) — word left
 bindkey '^[f' forward-word       # Option+Right (ESC+f) — word right
 
 # DELETION
-bindkey '^U'      backward-kill-line  # Cmd+Delete — delete to beginning of line
-bindkey '^[^?'    backward-kill-word  # Option+Backspace — delete word left
-bindkey '^[[3;3~' kill-word           # Fn+Option+Delete — delete word right
-bindkey '^[[3;9~' kill-line           # Fn+Cmd+Delete — delete to end of line
+bindkey '^U'   backward-kill-line  # Cmd+Delete — delete to beginning of line
+bindkey '^[^?' backward-kill-word  # Option+Backspace — delete word left
