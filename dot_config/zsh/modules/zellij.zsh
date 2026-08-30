b130d18f402d98d0e00009293713459a5fcfaea9
kir-sth <kir.sth@yandex.ru>
2026-08-30 16:28:46 +0300

    feat: improve zellij session workflows

--- dot_config/zsh/modules/zellij.zsh
+++ dot_config/zsh/modules/zellij.zsh
@@ -3,12 +3,22 @@ alias zl="zellij list-sessions"
 alias zka="zellij kill-all-sessions --yes"
 alias zda="zellij delete-all-sessions --yes"
 
-za() {
-  local session
+_z_execute_with_picker() {
+  local session action
+  action="$1"
 
   session=$(
     zellij list-sessions --short | gum choose
   ) || return
 
-  zellij attach "$session"
+  zellij "$action" "$session"
+}
+
+za() { _z_execute_with_picker "attach"; }
+zk() { _z_execute_with_picker "kill-session"; }
+zd() { _z_execute_with_picker "delete-session"; }
+
+zy() {
+    local target="${1:-.}"
+    zellij --layout code options --default-cwd "$target"
 }
